9720d1a205d2ebef9ac3f2abe327c7c1fdaa30cc
zaimrahman652-tech <zaimrahman652@gmail.com>
2026-05-14 15:06:12 +0700

    feat: fungsi push (deklarasi)

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -26,13 +26,4 @@ public:
     }
 
      int push(int value)
-    {
-        Node *newNode = new Node();
-        newNode->data = value;
-        newNode->next = top;
-        top = newNode;
-        cout << "Push value: " << value << endl;
-        return value;
-    }
-
     
