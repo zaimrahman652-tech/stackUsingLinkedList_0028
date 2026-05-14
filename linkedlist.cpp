a06e277e651bd320845834571e3e3367780c5564
zaimrahman652-tech <zaimrahman652@gmail.com>
2026-05-14 15:06:56 +0700

    eat: fungsi pop (deklarasi)

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -26,4 +26,14 @@ public:
     }
 
      int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
+    };
+
+     void pop()
     
