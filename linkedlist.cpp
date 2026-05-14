481c9421756395b5042f569befb76dbd5a8a1879
zaimrahman652-tech <zaimrahman652@gmail.com>
2026-05-14 15:03:25 +0700

    feat: tambah fungsi push

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -24,3 +24,15 @@ public:
     {
         top = NULL;
     }
+
+     int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
+    }
+
+    
