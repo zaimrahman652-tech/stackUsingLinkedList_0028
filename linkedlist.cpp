87161baff74d0ba286cf5c06222834696b00d710
zaimrahman652-tech <zaimrahman652@gmail.com>
2026-05-14 15:13:16 +0700

    feat: case pop

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -94,4 +94,14 @@ int main()
             cout << "Enter the value to push: ";
             cin >> value;
             stack.push(value);
-           
+            break;
+        case 2:
+            if (!stack.isEmpty())
+            {
+                stack.pop();
+            }
+            else
+            {
+                cout << "Stack is empty. Cannot pop." << endl;
+            }
+          
