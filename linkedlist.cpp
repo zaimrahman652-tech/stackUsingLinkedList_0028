3a969bcbb42711f20f6ede2590f9e3597b049837
zaimrahman652-tech <zaimrahman652@gmail.com>
2026-05-14 15:07:26 +0700

    feat: fungsi pop (implementasi)

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -36,4 +36,14 @@ public:
     };
 
      void pop()
-    
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty." << endl;
+        }
+
+        Node *temp = top;
+        top = top->next;
+        cout << "Popped value: " << top->data << endl;
+        delete temp;
+    }
