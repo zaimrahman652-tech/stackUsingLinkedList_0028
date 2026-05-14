9bebce5a265a0e2c3a9fb3b617521784a20d83e8
zaimrahman652-tech <zaimrahman652@gmail.com>
2026-05-14 15:13:51 +0700

    feat: case peek

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -104,4 +104,15 @@ int main()
             {
                 cout << "Stack is empty. Cannot pop." << endl;
             }
-          
+            break;
+        case 3:
+            if (!stack.isEmpty())
+            {
+                stack.peek();
+            }
+            else
+            {
+                cout << "Stack is empty.No top value." << endl;
+            }
+            break;
+         
